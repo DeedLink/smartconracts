ff64cd8ccc2cfc91ea91a7a10dd2df1585dd3262
VishSeran <vishwa.sovis@gmail.com>
2025-07-28 20:08:43 +0530

    Deed Structs and Deed NFT

--- contracts/libraries/DeedStruct.sol
+++ contracts/libraries/DeedStruct.sol
@@ -1,13 +1,40 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License_Identifier: MIT
 pragma solidity ^0.8.0;
 
-library Deedstructs{
+library DeedStructs {
     struct Coordinate {
         uint256 longitude;
-        uint256 latitude;  
+        uint256 latitude;
     }
 
+    struct OwnershipRecord {
+        address owner;
+        uint256 share;
+        uint256 timestamp;
+        string eventType;  // "INITIAL", "TRANSFER", "FRACTIONALIZED", "REDEEMED", "SHARE_TRANSFER"
+    }
 
+    struct DeedInfo {
+        uint256 tokenId;
+        Coordinate[] location;
+        uint256 area;
+        string deedNumber;
+        string ipfsHash;
+        string notary;
+        uint256 dateRegistered;
+        bool isRegistered;
+        bool isTokenized;
+        address tokenizedContractor;
+    }
+
+    struct ShareTransferRecord {
+        address from;
+        address to;
+        uint256 amount;
+        uint256 timestamp;
+    }
 
     
+
+
 }
